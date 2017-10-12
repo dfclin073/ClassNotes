418b34a8b1f20de1839f543c7e3ab12d05d7e0df
root <root@localhost.localdomain>
2017-10-11 22:30:07 -0400

    test2

--- prep.sh
+++ prep.sh
@@ -3,12 +3,18 @@ apt-get update
 apt-get install terminator
 apt-get install x11vnc
 echo "what is the name of the op?"
-read dirname
-mkdir /cline_$(date +%F)-$dirname
+read opname
+dirname = /cline_$(date +%F)-$opname
+mkdir /$dirname
+cd /tmp
+git clone git://github.com/dfclin073/tten.git/
+cp /tmp/tten/* /$dirname
+cd /$dirname
 
-git files
 
 
-echo "use this to connect to vnc server vinagre 10.50.25.# 5900"
-xterm -e x11vnc -o vnclog&
 
+echo "use this to connect to vnc server: vinagre 10.50.25.# 5900"
+xterm -hold -e x11vnc -o vnclog&
+terminator -T opnotes --working-directory /root/Downloads
+terminator -T OPS --working-directory /root/Downloads
